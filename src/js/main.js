9de25af95d00d6a6e0893bc5af7ff9e13d7a07de
Zemian Deng <zemian.deng@oracle.com>
2020-03-31 12:01:27 -0400

    Cleanup main.js to only setup OJET dep

--- src/js/main.js
+++ src/js/main.js
@@ -49,30 +49,30 @@
   );
 }());
 
-/**
- * A top-level require call executed by the Application.
- * Although 'knockout' would be loaded in any case (it is specified as a  dependency
- * by some modules), we are listing it explicitly to get the reference to the 'ko'
- * object in the callback
- */
-require(['ojs/ojbootstrap', 'knockout', 'appController', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojtoolbar', 'ojs/ojmenu'],
-  function (Bootstrap, ko, app) { // this callback gets executed when all required modules are loaded
-
-      Bootstrap.whenDocumentReady().then(
-        function() {
-          function init() {
-            // Bind your ViewModel for the content of the whole page body.
-            ko.applyBindings(app, document.getElementById('globalBody'));
-          }
-
-          // If running in a hybrid (e.g. Cordova) environment, we need to wait for the deviceready
-          // event before executing any code that might interact with Cordova APIs or plugins.
-          if (document.body.classList.contains('oj-hybrid')) {
-            document.addEventListener("deviceready", init);
-          } else {
-            init();
-          }
-        }
-      );
-    }
-);
+// /**
+//  * A top-level require call executed by the Application.
+//  * Although 'knockout' would be loaded in any case (it is specified as a  dependency
+//  * by some modules), we are listing it explicitly to get the reference to the 'ko'
+//  * object in the callback
+//  */
+// require(['ojs/ojbootstrap', 'knockout', 'appController', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojtoolbar', 'ojs/ojmenu'],
+//   function (Bootstrap, ko, app) { // this callback gets executed when all required modules are loaded
+//
+//       Bootstrap.whenDocumentReady().then(
+//         function() {
+//           function init() {
+//             // Bind your ViewModel for the content of the whole page body.
+//             ko.applyBindings(app, document.getElementById('globalBody'));
+//           }
+//
+//           // If running in a hybrid (e.g. Cordova) environment, we need to wait for the deviceready
+//           // event before executing any code that might interact with Cordova APIs or plugins.
+//           if (document.body.classList.contains('oj-hybrid')) {
+//             document.addEventListener("deviceready", init);
+//           } else {
+//             init();
+//           }
+//         }
+//       );
+//     }
+// );
